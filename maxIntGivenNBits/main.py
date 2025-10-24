c0fae024d5ea381552cc29afaa7b1f5614296990
Sparse808 <Sparse.dev@gmail.com>
2025-10-24 15:43:53 -0400

    selection implemented

--- maxIntGivenNBits/main.py
+++ maxIntGivenNBits/main.py
@@ -11,6 +11,26 @@ def createPop(popSize: int, n: int) -> list[list[int]]:
         population.append(choices([0,1], k=n))
     return population
 
+def findFitness(population: list[list[int]]) -> list[int]:
+    fitness = []
+    for item in population:
+        fitness.append(int("".join(str(bit) for bit in item),2))
+    return fitness
+
+def createPool(population: list[list[int]], fitnessLookup: list[int], tournSize: int, popSize: int) -> list[list[int]]:
+    matingPool = []
+
+    for _ in range(popSize):
+        indices = choices(range(popSize), k=tournSize)
+        maxFitness = fitnessLookup[indices[0]]
+        maxIndex = indices[0]
+        for index in indices:
+            if fitnessLookup[index] >= maxFitness:
+                maxFitness = fitnessLookup[index]
+                maxIndex = index
+        matingPool.append(population[maxIndex])
+
+    return matingPool
 
 def main():
     print("starting")
@@ -22,12 +42,12 @@ def main():
     population = createPop(popSize, n)
     printChomo(population)
 
-
-    matingPool = []
-
-
+    fitnessLookup = findFitness(population)
+    printChomo(fitnessLookup)
 
 
+    matingPool = createPool(population, fitnessLookup, tournSize, popSize)
+    printChomo(matingPool)
 
 if __name__ == "__main__":
     main()
